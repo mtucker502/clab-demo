f35cdfd7186d2d48e69239b09783477d415ed67c
mtucker502 <github@netzolt.com>
2025-04-23 04:44:28 +0000

    add vsrx lab example

---
+++ lab4/run_tests.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+. ~/venv/bin/activate
+ansible-playbook pb.write.resource.files.yaml
+docker run -v $PWD/test:/example --network clab --rm -it --name nita-robot juniper/nita-robot:23.12-ssh /bin/bash -c "cd /example;robot tests/"
